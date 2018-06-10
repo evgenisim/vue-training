d105dfa8034f77794844b5e5bb39300434c9bdd7
Evgeni Simeonov <evgenisim93@gmail.com>
2018-06-10 22:50:57 +0300

    Advanced component usage

---
+++ 9-advanced-components-usage/src/components/Quote.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<div class="title">
+			<slot name="title"></slot>
+			<span>
+				<slot name="subtitle">The subtitle</slot>
+			</span>
+		</div>
+
+		<div class="content">
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['quote']
+	}
+</script>
+
+<style scoped>
+	div {
+		border: 1px solid  #ccc;
+		width: 100%; 
+        box-shadow: 1px 1px 2px #000;
+        padding: 30px; 
+        margin: 30px auto; 
+        text-align: center; 
+	}
+
+	h2 {
+		color: red;
+	}
+
+	.title {
+		font-style: italic;
+	}
+</style>
